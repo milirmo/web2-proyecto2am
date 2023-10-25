adafddcd02cf125c0eef62202b751baab26be399
milirmo <110854465+milirmo@users.noreply.github.com>
2023-10-25 17:06:10 -0300

    Add files via upload

---
+++ src/app/componentes/productos/productos.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos',
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.css']
+})
+export class ProductosComponent {
+    peliculas=[
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      },
+      {
+        img:"",
+        nombre:"",
+        año:""
+
+      }
+    ]
+}
